904aa25211b36f74c0357c0b5328e104f3a4073b
Souma <souma.hitech@gmail.com>
2020-08-08 15:42:32 +0530

    plug in mongoDB with list page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const { urlencoded } = require("body-parser");
 const date = require(__dirname+"/date.js");
 
@@ -15,16 +16,60 @@ app.use(bodyParser.urlencoded({extended:true}));
 // this is a static resource or public resource
 app.use(express.static("public"));
 
-// global veriable
+//connect mongodb database 
+mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true,useUnifiedTopology: true});
+// we create a item schema
+const itemsSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    }
+});
+
+// create a model
+const Item = new mongoose.model("Item",itemsSchema);
+//create 3 document for default 3 item
+const item1 = new Item({
+    name:"Wellcome to todo list"
+});
+
+const item2 = new Item({
+    name:"Click + to add new list item"
+});
+
+const item3 = new Item({
+    name:"Press - to delete list item"
+});
+// default item array
+const defaultItems = [item1,item2,item3];
+
+/*// global veriable
 // initially we set these 3 item 
-let items =["Buy Food","Cook Food","Eat Food"];
-let work =[];
+let items =["Buy Food","Cook Food","Eat Food"];*/
+let work = [];
 
 // home route get request
 app.get("/",function(req,res){
     let day = date.getDate();//call the getDate function using date module
-    // here we pass current date as a list title and a array 
-    res.render("list",{listTitle:day,newListItems:items});
+    //fetch all data and put into ejs template and send file
+    Item.find({},function(err,foundItem){
+    
+        if(foundItem.length === 0){
+            //insert all of default item into our items collection
+            Item.insertMany(defaultItems,function(err){
+                if(err)
+                    console.log(err);
+                else
+                    console.log("Succesfully default item are saved into DB.");
+            });
+            //saved default item and redirect to / route
+            res.redirect("/");
+        }else{
+            res.render("list",{listTitle:day,newListItems:foundItem});
+        }
+        
+    });
+    
 });
 
 // work route get get requset
@@ -58,11 +103,14 @@ app.post("/",function(req,res){
     }
     else{
         if(item!==""){
-            items.push(item);
+            //insert item into database
+            const newItem = new Item({name:item});
+            newItem.save();
          }
         // console.log(item);
         res.redirect("/");
     }
+    
 });
 
 
